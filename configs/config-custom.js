42546fbac358ad1ef860213f1aa5da44bc0bdfb3
whatisthisatrocity <248923470+whatisthisatrocity@users.noreply.github.com>
2026-04-06 11:20:53 +0100

    add config

---
+++ configs/config-custom.js
@@ -0,0 +1,25 @@
+let map = {
+    "selectable": true,
+    "MODE": "tdm",
+    "serverName": "Nuclear War",
+    "TEAM_AMOUNT": 2,
+    "WIDTH": 8000,
+    "HEIGHT": 2000,
+    "X_GRID": 8,
+    "Y_GRID": 4,
+    "MAX_FOOD": 1000,
+    "MAX_NEST_FOOD": 0,
+    "MAX_CRASHERS": 0,
+    "MAX_SANCS": 2,
+    "ROOM_SETUP": [
+        ["bas1","norm","norm","norm","norm","norm","norm","bas2"],
+        ["bas1","norm","norm","norm","norm","norm","norm","bas2"],
+        ["bas1","norm","norm","norm","norm","norm","norm","bas2"],
+        ["bas1","norm","norm","norm","norm","norm","norm","bas2"],
+    ],
+    "BOTS": 50,
+    "BORDER_FORCE": 1, // 1 normally
+    "BOSS_SPAWN_TIMER": 32 // 32 normally
+}
+
+map
